c983c7f7f0f41223f06c342b74905215655df79e
rooot13 <mesutdincer.altun@ogr.deu.edu.tr>
2024-04-23 04:06:36 +0300

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   require('dotenv').config({ path: './src/.env' });
 }
-
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 var port = 3000
 
 const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views',__dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('puclic'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
+
 
 
 const mongoose = require('mongoose') 
@@ -24,6 +27,7 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorsRouter)
 
 app.listen(process.env.PORT || port, (req, res) => {
   console.log(`Port listening on ${port}`)
